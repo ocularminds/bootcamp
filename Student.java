41a4c2876af18dca71b258f01c2615efa0da66e6
Sholasaka101231 <sholasaka@yahoo.com>
2023-08-20 20:49:50 +0100

    Created a Student class

--- Student.java
+++ Student.java
@@ -1,7 +1,9 @@
 import java.util.HashMap;
 import java.util.ArrayList;
 public class Student {
+   
     HashMap<String,List> studentrecord ;
+    
     public Student(){
             new = new Map();
     }
