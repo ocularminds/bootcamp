a4f9aa7e3a62f19a29194955547b9a5b37f7d2d5
Philomina Oritseminomi Ejegi Ede <pe222ns@student.lnu.se>
2023-08-20 22:20:30 +0200

    Imported java utilities

--- Student.java
+++ Student.java
@@ -1,8 +1,12 @@
-public class Students{
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Students {
     Map<String, List> students;
 
-    public Students(){
+    public Students() {
         students = new HashMap<>();
-        
+
     }
 }
