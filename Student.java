fe7cf00e01bcd73cea6b3eb25a062e81f23249a7
Sholasaka101231 <sholasaka@yahoo.com>
2023-08-20 21:13:51 +0100

    updated a student class

--- Student.java
+++ Student.java
@@ -1,11 +1,12 @@
 import java.util.HashMap;
+import java.util.Map;
 import java.util.ArrayList;
 public class Student {
    
     HashMap<String,List> studentrecord ;
     
     public Student(){
-            new = new Map();
+           studentrecord  = new Map<studentrecord>();
     }
     
 }
