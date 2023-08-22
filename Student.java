d04f625089539c1293859af1817f3362b6e22006
Sholasaka101231 <sholasaka@yahoo.com>
2023-08-23 00:39:15 +0100

    add students class

--- Student.java
+++ Student.java
@@ -3,11 +3,29 @@ import java.util.Map;
 import java.util.ArrayList;
 import java.util.List;
 public class Student {
-   
+    
     Map<String,List> student ;
     
     public Student(){
            student = new HashMap<>();
     }
+    public ArrayList<String> addStudentRecords(String firstName, String LastName, String department, String hostel, String cgpa){
+        ArrayList<String> studentsRecord = new ArrayList<>();
+        studentsRecord.add(firstName);
+        studentsRecord.add(LastName);
+        studentsRecord.add(department);
+        studentsRecord.add(hostel);
+        studentsRecord.add(cgpa);
+        return studentsRecord;
+        
+    }
+    public static void main(String[] args) {
+        Student st = new Student();
+       System.out.println(st.addStudentRecords("Saka","Shola","Maths","Green","3.0"));
+
+    }
+     
     
-}
+    }
+    
+
