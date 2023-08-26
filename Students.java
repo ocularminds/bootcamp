502a74fa2a4292b4f4bd96133076d0b369c7ffb7
Philomina Oritseminomi Ejegi Ede <pe222ns@student.lnu.se>
2023-08-26 15:35:03 +0200

    Code refactoring

--- Students.java
+++ Students.java
@@ -7,27 +7,26 @@ import java.util.Map;
  * Creating Student class that hold information of students.
  */
 public class Students {
-    Map<String, List> students;
+    Map<String, List<String>> students;
 
     public Students() {
         students = new HashMap<>();
-
     }
 
     /*
      * Formulate students'record.
      */
     public void add(String firstName, String lastName, String department, String level,
-            String hostel, String cgpaRecord) {
+            String number, String cgpa) {
         List<String> record = new ArrayList<>();
+        record.add(number);
         record.add(firstName);
         record.add(lastName);
         record.add(department);
         record.add(level);
-        record.add(hostel);
-        record.add(cgpaRecord);
+        record.add(cgpa);
 
-        students.put(generateStudentNumber(), record);
+        students.put(number, record);
     }
 
     public List<String> getStudent(String studentNumber) {
@@ -38,19 +37,17 @@ public class Students {
      * Print out student record in as a String.
      */
 
-    public void main(String[] args) {
-        String firstName = "Mike";
-        String lastName = "Olushola";
-        String department = "Civil engineering";
-        String level = "3rd Year";
-        String hostel = "MaryLand Hostel";
-        String cpgaRecord = "3.8pt";
-
-        List<String> studentRecord;
-        System.out.println(String.join("," studentRecord));
-
-
-
-
+    public void print(List<String> record) {
+        for (String item : record) {
+            // for(int x = 0; x < record.size(); x++)
+            // System.out.println(item);
+            System.out.print(item + "\t");
+        }
     }
+
+    public static void main(String[] args) {
+
+        Students students = new Students();
+    }
+
 }
