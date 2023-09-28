6f9f4f65c40b985c0c7683ad9ba060251c772e6e
Philomina Oritseminomi Ejegi Ede <pe222ns@student.lnu.se>
2023-09-28 14:50:26 +0200

    Connected MySQL to Bootcamp

---
+++ database/src/main/java/bootcamp/edu/App.java
@@ -0,0 +1,5 @@
+package bootcamp.edu;
+
+public class App {
+    
+}
---
+++ database/src/main/java/bootcamp/edu/Student.java
@@ -0,0 +1,7 @@
+package bootcamp.edu;
+
+public class Student {
+    private String name;
+    private Address address;
+    
+}
